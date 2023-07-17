808fbd42593e31f291d64e1074eb08fb394ec9a0
Montigny Gérald <131920955+Slam85@users.noreply.github.com>
2023-07-17 15:04:40 +0200

    controller2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,25 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function create()
+    {
+        return view('create-user');
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'firstName' => 'required',
+            'lastName' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:6',
+            'confpassword' => 'required|same:password',
+            'username' => 'required',
+            'date_of_birth' => 'required|date|before_or_equal:18 years ago',
+            'picture' => 'image|max:2048',
+        ]);
+
+
+        return redirect('/connection')->with('success', 'Compte créé avec succès !');
+    }
 }
