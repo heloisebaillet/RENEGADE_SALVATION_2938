600105df1fa46c21c03820a65aea94880e4f05b0
XavierFtn <xavier.fontaine072@gmail.com>
2023-07-28 13:56:34 +0200

    Modif migration delete

--- database/migrations/2023_07_17_083210_create_users_table.php
+++ database/migrations/2023_07_17_083210_create_users_table.php
@@ -26,6 +26,9 @@ return new class extends Migration
             $table->string('picture', 255)->nullable();
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+
+            $table->foreignIdFor(Users::class)
+                ->onDelete('cascade');
         });
     }
 
