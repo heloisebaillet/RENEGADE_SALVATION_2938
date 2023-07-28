19b441341f18cf70542d0caf0f1ef056a4b0bdb3
Jimmy <131156471+Jimbow-cl@users.noreply.github.com>
2023-07-28 21:46:26 +0200

    History end

--- app/Http/Controllers/RoundController.php
+++ app/Http/Controllers/RoundController.php
@@ -10,24 +10,12 @@ class RoundController extends Controller
     public function read()
     {
         $user_id = Auth::user()->id;
-        $uuidObject = Round::select('uuid')->where('user_id', $user_id)->first();
-
-        if (!$uuidObject) {
-    
-            return response()->json(['error' => 'No Battle Found'], 404);
-        }
-
-        $uuid = $uuidObject->uuid;
-
-        $rounds = Round::select('planetary_system_name', 'user_id','is_defender','nb_fighter','nb_frigate','nb_cruiser','nb_destroyer','nb_round','rounds.created_at')
-        ->where('uuid', $uuid)
-        ->leftJoin('users', 'users.id', '=', 'rounds.user_id')
-        ->get();
-
-
+       
+        $round = Round::where('user_id', $user_id)->get();
+  
 
         return response()->json([
-            'round' => $rounds
+            'round' => $round
         ], 200);
     }
 }
