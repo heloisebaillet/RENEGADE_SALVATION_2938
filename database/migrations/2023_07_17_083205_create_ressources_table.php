02053c7bcaf232f2b1f5f73dd329ae1deb38b14a
Jimmy <131156471+Jimbow-cl@users.noreply.github.com>
2023-07-30 09:35:55 +0200

    Cascade

--- database/migrations/2023_07_17_083205_create_ressources_table.php
+++ database/migrations/2023_07_17_083205_create_ressources_table.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
@@ -14,7 +14,10 @@ return new class extends Migration
     {
         Schema::create('ressources', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id');
+            $table->foreignIdFor(User::class)
+                ->constrained()
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
             $table->string('type');
             $table->integer('quantity');
             $table->timestamp('created_at')->useCurrent();
